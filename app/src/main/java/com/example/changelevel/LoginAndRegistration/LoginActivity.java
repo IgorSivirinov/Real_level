4fd84c8decfbaab7f81411155c1b546474dcf44a
IgorSivirinov <igorek7937@gmail.com>
2020-05-20 15:08:18 +0200

    0.2.1

--- app/src/main/java/com/example/changelevel/LoginAndRegistration/LoginActivity.java
+++ app/src/main/java/com/example/changelevel/LoginAndRegistration/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.example.changelevel.LoginAndRegistration;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.changelevel.API.Api;
 import com.example.changelevel.API.Constantes;
@@ -15,6 +23,13 @@ import com.example.changelevel.R;
 import com.example.changelevel.User.User;
 import com.example.changelevel.helper.SessionManager;
 import com.example.changelevel.models.DefaultResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,43 +38,106 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText loginMail, loginPassword;
-    private Button buttonLogin;
+    private FirebaseAuth mAuth;
+    private TextInputLayout email, password;
+    private TextInputEditText emailTIET, passwordTITE;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginMail = findViewById(R.id.loginMail);
-        loginPassword = findViewById(R.id.loginPassword);
-        buttonLogin = findViewById(R.id.buttonLogin);
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.til_email_activity_login);
+        password = findViewById(R.id.til_password_activity_login);
+        progressBar = findViewById(R.id.pb_activity_login);
+
+        clearErrorEditText();
+            findViewById(R.id.buttonLogin).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    if(checkEmptyString())
+                        mAuth.signInWithEmailAndPassword(email.getEditText().getText().toString().trim(),
+                                password.getEditText().getText().toString().trim())
+                                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            password.setError(null);
+                                            FirebaseUser user = mAuth.getCurrentUser();
+                                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                            startActivity(intent);
+                                        } else {
+                                            password.setError("Не верный пароль или почта");
+                                            progressBar.setVisibility(View.INVISIBLE);
+                                        }
+                                    }
+                                });
+                    else progressBar.setVisibility(View.INVISIBLE);
+                }
+            });
+            findViewById(R.id.button_go_to_registration).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+        }
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser!=null){
+//            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//            startActivity(intent);
+//        }
+//    }
+    private void clearErrorEditText(){
+        email.getEditText().addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                Retrofit retrofit = new Retrofit.Builder().baseUrl(Constantes.BASE_URL)
-                        .addConverterFactory(GsonConverterFactory.create()).build();
-                Api service = retrofit.create(Api.class);
-                Call<DefaultResponse> call = service.Login(
-                        loginMail.getText().toString()      ,
-                        loginPassword.getText().toString());
-                call.enqueue(new Callback<DefaultResponse>() {
-                    @Override
-                    public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response) {
-                        if(!response.body().isErr()){
-                            User user = response.body().getUser();
-                            SessionManager.getInstance(getApplicationContext()).saveUser(user);
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            intent.putExtra("user", user);
-                            startActivity(intent);
-                        }
-                    }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                email.setError(null);
+            }
 
-                    @Override
-                    public void onFailure(Call<DefaultResponse> call, Throwable t) {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-                    }
-                });
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        password.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                password.setError(null);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
 
             }
         });
     }
+    private boolean checkEmptyString(){
+        if (email.getEditText().getText().toString().trim().isEmpty()) email.setError("Пустое поле");
+            else email.setError(null);
+        if(password.getEditText().getText().toString().trim().isEmpty()) password.setError("Пустое поле");
+            else password.setError(null);
+        return !email.getEditText().getText().toString().trim().isEmpty() && !password.getEditText().getText().toString().trim().isEmpty();
+    }
+
+
 }
--- app/src/main/java/com/example/changelevel/LoginAndRegistration/RegistrationActivity.java
+++ app/src/main/java/com/example/changelevel/LoginAndRegistration/RegistrationActivity.java
@@ -1,12 +1,17 @@
 package com.example.changelevel.LoginAndRegistration;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.changelevel.API.Api;
 import com.example.changelevel.API.Constantes;
@@ -15,6 +20,12 @@ import com.example.changelevel.R;
 import com.example.changelevel.User.User;
 import com.example.changelevel.helper.SessionManager;
 import com.example.changelevel.models.DefaultResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,46 +34,147 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegistrationActivity extends AppCompatActivity {
-    private Button buttonRegistration;
-    private EditText registrationMail, registrationUserName, registrationPassword, registrationRepeatPassword;
+    private FirebaseAuth mAuth;
+    private TextInputLayout name, email, password, rePassword;
+    private ProgressBar progressBar;
+    private boolean checkPasswords, checkEmptyString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        registrationMail = findViewById(R.id.registrationMail);
-        registrationUserName = findViewById(R.id.registrationUserName);
-        registrationPassword = findViewById(R.id.registrationPassword);
-        registrationRepeatPassword = findViewById(R.id.registrationRepeatPassword);
-        buttonRegistration = findViewById(R.id.buttonRegistration);
-        buttonRegistration.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+        name = findViewById(R.id.til_name_activity_registration);
+        email = findViewById(R.id.til_email_activity_registration);
+        password = findViewById(R.id.til_password_activity_registration);
+        rePassword = findViewById(R.id.til_re_password_activity_registration);
+
+        progressBar = findViewById(R.id.pb_activity_registration);
+        findViewById(R.id.ib_back_activity_registration).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (registrationPassword.getText().toString().equals(registrationRepeatPassword.getText().toString())){
-                    Retrofit retrofit=new Retrofit.Builder().baseUrl(Constantes.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
-                    Api service= retrofit.create(Api.class);
-                    Call<DefaultResponse> call=service.Register(registrationUserName.getText().toString(),
-                            registrationMail.getText().toString(),
-                            registrationPassword.getText().toString());
-                    call.enqueue(new Callback<DefaultResponse>() {
-                        @Override
-                        public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response) {
-                            if(!response.body().isErr())
-                            {
-                                User user = response.body().getUser();
-                                SessionManager.getInstance(getApplicationContext()).saveUser(user);
-                                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
-                                intent.putExtra("user", user);
-                                startActivity(intent);
-                            }
-                        }
+                finish();
+            }
+        });
+        clearErrorEditText();
+        findViewById(R.id.button_registration).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkEmptyString = false;
+                checkPasswords = false;
+                progressBar.setVisibility(View.VISIBLE);
+                isCheckEmptyString();
+                    if (password.getEditText().getText().toString().trim()
+                            .equals(rePassword.getEditText().getText().toString().trim())) {
+                    checkPasswords=true;
+                    }else rePassword.setError("Пароли не совпадают");
 
-                        @Override
-                        public void onFailure(Call<DefaultResponse> call, Throwable t) {
+                    if(checkPasswords && checkEmptyString) {
+                        mAuth.createUserWithEmailAndPassword(
+                                email.getEditText().getText().toString().trim(),
+                                password.getEditText().getText().toString().trim())
+                                .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            rePassword.setError(null);
+                                            FirebaseUser user = mAuth.getCurrentUser();
+                                            Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+                                            startActivity(intent);
+                                        } else {
+                                            rePassword.setError("Ошибка регистрации");
+                                            progressBar.setVisibility(View.INVISIBLE);
+                                        }
+                                    }
+                                });
+                    } else progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
 
-                        }
-                    });
 
-                }
+
+    private void isCheckEmptyString(){
+        if (name.getEditText().getText().toString().trim().isEmpty()) name.setError("Пустое поле");
+            else name.setError(null);
+        if (email.getEditText().getText().toString().trim().isEmpty()) email.setError("Пустое поле");
+            else email.setError(null);
+        if (password.getEditText().getText().toString().trim().isEmpty()) password.setError("Пустое поле");
+            else password.setError(null);
+        if (rePassword.getEditText().getText().toString().trim().isEmpty()) rePassword.setError("Пустое поле");
+            else rePassword.setError(null);
+        checkEmptyString = (!name.getEditText().getText().toString().trim().isEmpty()
+                && !email.getEditText().getText().toString().trim().isEmpty()
+                    && !password.getEditText().getText().toString().trim().isEmpty()
+                        && !rePassword.getEditText().getText().toString().trim().isEmpty());
+
+
+    }
+
+    private void clearErrorEditText(){
+        name.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                name.setError(null);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        email.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                email.setError(null);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        password.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                password.setError(null);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        rePassword.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                rePassword.setError(null);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
 
             }
         });
